34f41803bce6024a6095cbbf2992c2fd8a309117
Delta <granjapenadiego82@gmail.com>
2026-05-03 16:01:37 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+
+export default function App() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) return <View />;
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>Necesitamos tu permiso para acceder a la cámara</Text>
+        <Button onPress={requestPermission} title="Conceder permiso" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => current === 'back' ? 'front' : 'back');
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Voltear cámara</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center' },
+  message: { textAlign: 'center', paddingBottom: 10 },
+  camera: { flex: 1 },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: { flex: 1, alignSelf: 'flex-end', alignItems: 'center' },
+  text: { fontSize: 24, fontWeight: 'bold', color: 'white' },
+});
